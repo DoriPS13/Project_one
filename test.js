82237e4c44431538323ebd35c0116b180f18b183
DoriPS13 <palmer.sanderson@gmail.com>
2014-08-06 21:02:48 -0700

    cool button onlocation

--- test.js
+++ test.js
@@ -20,7 +20,37 @@ mclient.search({name:'ramen', locality: "San Francisco"}, function(result){
 
 
 
+<script>
 
+	L.mapbox.accessToken = 'pk.eyJ1IjoiZG9yaXBzMTMiLCJhIjoic0FNRE51byJ9.SSNLexDQmGPKXiJ1TH4lvw';
+	var map = L.mapbox.map('map', 'examples.map-20v6611k')
+	    .setView(['<%= ramen.venue.lat %>', '<%=ramen.venue.long %>'], 16);
+
+	L.mapbox.featureLayer({
+	    // this feature is in the GeoJSON format: see geojson.org
+	    // for the full specification
+	    type: 'Feature',
+	    geometry: {
+	        type: 'Point',
+	        // coordinates here are in longitude, latitude order because
+	        // x, y is the standard for GeoJSON and many formats
+	        coordinates: [
+	         '<%=ramen.venue.long %>', 
+	         '<%= ramen.venue.lat %>'
+	        ]
+	    },
+	    properties: {
+	        title:'<%= ramen.venue.name %>',
+	        description:'<%= ramen.venue.street_address %>', 
+	        // one can customize markers by adding simplestyle properties
+	        // https://www.mapbox.com/foundations/an-open-platform/#simplestyle
+				    "marker-symbol": "star",
+				    "marker-size": "medium",
+				    "marker-color": "#f44"
+	    }
+	}).addTo(map);
+}
+</script>
 
 
 
