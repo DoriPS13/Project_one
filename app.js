631924a715709d5d6ddbbe781418e31a502e6fc3
palmersndrsn <palmer.sanderson>
2014-10-15 15:06:21 -0700

    cleaning up indentation

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var express = require('express'),
 		locu = require('locu'),
 		app = express(),
 		db = require('./models/index');
-		
+
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extend: true}));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.use(cookieSession({
 	secret: 'thisIsTheSecretKey',//to be changed
@@ -27,8 +27,8 @@ app.use(flash());
 
 //for checking if user has authori
 passport.serializeUser(function(user,done) {
-	done(null,user.id)
-})
+	done(null,user.id);
+});
 
 passport.deserializeUser(function(id,done) {
 	db.user.find({
@@ -38,25 +38,25 @@ passport.deserializeUser(function(id,done) {
 	}).done(function(error,user) {
 		//slightly confusing
 		done(error,user);
-	})
-})
+	});
+});
 //ramen search by location need to add if error
-var mclient = new locu.MenuItemClient(process.env.locuKey); 
+var mclient = new locu.MenuItemClient(process.env.locuKey);
 
 var ramenSearchLoc = function(long, lat, callback) {
   mclient.search({name:'ramen', location:[long, lat]}, function(result){
-  	var ramenResults = result.objects
-    console.log('running!')
-     	callback.call(ramenResults)
+  	var ramenResults = result.objects;
+    console.log('running!');
+     	callback.call(ramenResults);
 	});
 };
 
 
 var ramenSearch = function(loc, callback) {
   mclient.search({name:'ramen', locality: loc}, function(result){
-  	var ramenResults = result.objects
-    callback.call(ramenResults)
-   
+  	var ramenResults = result.objects;
+    callback.call(ramenResults);
+
 	});
 };
 
@@ -67,20 +67,19 @@ app.post('/geo', function(req,res) {
 			ramenResults: this,
 			 isAuthenticated: req.isAuthenticated()
 		});
-	})
+	});
 });
 
 //login request
-
 app.post('/login', passport.authenticate('local', {
-  successRedirect: '/', 
-  failureRedirect: '/login', 
+  successRedirect: '/',
+  failureRedirect: '/login',
   failureFlash: true
 }));
 
 //homepage
 app.get('/', function(req,res) {
-	
+
 	res.render("home", {isAuthenticated: req.isAuthenticated(),
    });
 });
@@ -90,94 +89,92 @@ app.get('/signup', function(req,res) {
 	res.render('signup',
 	{ isAuthenticated: req.isAuthenticated(),
    });
-})
+});
 
+// create user
 app.post('/create', function(req,res) {
-	db.user.createNewUser(req.body.username, req.body.password, 
+	db.user.createNewUser(req.body.username, req.body.password,
 		function(success) {
-			console.log("Success!!")
-			res.redirect('/')
+			console.log("Success!!");
+			res.redirect('/');
 		},
 		function(err) {
-			console.log("ERRORED")
-			res.redirect("/signup")
-		})
-})
+			console.log("ERRORED");
+			res.redirect("/signup");
+		});
+});
 //search by location
 app.post('/search', function(req,res) {
-	console.log(req.body.location)
-	var loc = (req.body.location)
+	console.log(req.body.location);
+	var loc = (req.body.location);
 	ramenSearch(loc, function() {
 				// console.log(this)
 		res.render('results', {
 			ramenResults: this,
 			isAuthenticated: req.isAuthenticated()
 		});
-	})	
+	});
 });
-//results 
+
+//results
 app.get('/results', function(req,res) {
+});
 
-})
-
+// single ramen result
 app.get('/ramen/:id', function(req,res) {
-	var ramenId = req.params.id
+	var ramenId = req.params.id;
 	db.item.find({ where: {
 		locuId: ramenId }
 	}).success(function(ramenItem) {
-		var rating = 'N/A'
+		var rating = 'N/A';
 		if (ramenItem !== null) {
-			rating = ramenItem.rating
+			rating = ramenItem.rating;
 		}
-		mclient.request(ramenId, null, function (detailThing) { 
+		mclient.request(ramenId, null, function (detailThing) {
 			res.render('single_result', {
 				ramenResult: detailThing.objects,
 				rating: rating,
 				isAuthenticated: req.isAuthenticated()
-			})
+			});
 		});
 	});
-})
+});
 
 //rating
 app.post('/ramen/:locu_id', function(req, res){
 	if (req.user){
-		locuId = req.params.locu_id
+		locuId = req.params.locu_id;
 		db.item.findOrCreate({
 				locuId: locuId
 		}).success(function(item, created){
 			if(created){
-				item.rating = req.body.score
+				item.rating = req.body.score;
 				item.ratingCount = 1;
-				item.save()
+				item.save();
 			} else {
 				var oldRating = item.rating;
 				var oldCount = item.ratingCount;
 
 				var newRating;
-				newRating = (oldRating*oldCount + Number(req.body.score))/(oldCount + 1)
-
+				newRating = (oldRating*oldCount + Number(req.body.score))/(oldCount + 1);
 				item.rating = newRating;
 				item.ratingCount += 1;
 				item.save().success(function(item){
-					
-				})
+				});
 				req.user.addItem(item);
 			}
-			// res.(item)
-		})
-	 }
-})
-
+		});
+	}
+});
 
 //logout
 app.get('/logout', function(req,res){
   req.logout();
-  res.redirect('/')
+  res.redirect('/');
 });
 
 
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log('Systems Online on Port 3000 Captian')
-})
+	console.log('Systems Online on Port 3000 Captian');
+});
